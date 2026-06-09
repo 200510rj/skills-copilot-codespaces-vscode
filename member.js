9ecb1cd6e8fd5fea6396451b5d8e79e5544cfa3d
200510rj <167175692+200510rj@users.noreply.github.com>
2026-06-09 02:38:22 +0000

    add member.js whics has two funcion

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    console.log("This is a member function.");
+    return "Member function executed.";
+}
+//creat a function form which accect members info 
+function getMemberInfo(name, age) {
+    console.log("Getting member info...");
+    return `Name: ${name}, Age: ${age}`;
+}
